0610e17ba4660d71adf068d9dcfd502aedbe73bb
youichiro <cinnamon416@gmail.com>
2022-04-16 20:49:58 +0900

    学生登録フォーム

--- app/controllers/students/registrations_controller.rb
+++ app/controllers/students/registrations_controller.rb
@@ -1,18 +1,21 @@
 # frozen_string_literal: true
 
 class Students::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+    build_resource
+    resource.build_student_detail
+    yield resource if block_given?
+    respond_with resource
+  end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    super
+  end
 
   # GET /resource/edit
   # def edit
@@ -41,9 +44,13 @@ class Students::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+      student_detail_attributes: [
+        :name, :birthday, :school_name, :department
+      ]
+    ])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
