31c7d69cb6cbb13826fac539e39b4d3e1e2ca28e
youichiro <cinnamon416@gmail.com>
2022-04-16 17:32:54 +0900

    docker-compose run --rm web rails g devise Student, Employee, AdminUser

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :admin_users
+  devise_for :employees
+  devise_for :students
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
