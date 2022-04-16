64b1d157f8bc15c8c7b84eb885eacacb2fa5aba0
youichiro <cinnamon416@gmail.com>
2022-04-16 17:53:15 +0900

    日本語設定

--- config/application.rb
+++ config/application.rb
@@ -28,10 +28,12 @@ module Myapp
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.time_zone = "Tokyo"
+    config.i18n.default_locale = :ja
   end
 end
