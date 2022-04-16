bbb090131e8a717ad258759a0fd6e78149693b8c
youichiro <cinnamon416@gmail.com>
2022-04-16 18:42:56 +0900

    bin/rails g migration CreateBusinessCategory

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_16_094047) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_16_094229) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_16_094047) do
     t.index ["confirmation_token"], name: "index_admin_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "business_categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_business_categories_on_name", unique: true
   end
 
   create_table "employee_details", force: :cascade do |t|
