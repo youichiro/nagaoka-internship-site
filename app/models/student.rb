c0bdef2c6930c96932128a03045b1f311934ab13
youichiro <cinnamon416@gmail.com>
2022-04-16 17:36:01 +0900

    modify devise users schema

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,7 @@
 class Student < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :lockable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :timeoutable, :trackable
 end
--- db/migrate/20220416083054_devise_create_employees.rb
+++ db/migrate/20220416083054_devise_create_employees.rb
@@ -15,17 +15,17 @@ class DeviseCreateEmployees < ActiveRecord::Migration[7.0]
       t.datetime :remember_created_at
 
       ## Trackable
-      # t.integer  :sign_in_count, default: 0, null: false
-      # t.datetime :current_sign_in_at
-      # t.datetime :last_sign_in_at
-      # t.string   :current_sign_in_ip
-      # t.string   :last_sign_in_ip
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
 
       ## Confirmable
-      # t.string   :confirmation_token
-      # t.datetime :confirmed_at
-      # t.datetime :confirmation_sent_at
-      # t.string   :unconfirmed_email # Only if using reconfirmable
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      t.string   :unconfirmed_email # Only if using reconfirmable
 
       ## Lockable
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
@@ -38,7 +38,7 @@ class DeviseCreateEmployees < ActiveRecord::Migration[7.0]
 
     add_index :employees, :email,                unique: true
     add_index :employees, :reset_password_token, unique: true
-    # add_index :employees, :confirmation_token,   unique: true
+    add_index :employees, :confirmation_token,   unique: true
     # add_index :employees, :unlock_token,         unique: true
   end
 end
