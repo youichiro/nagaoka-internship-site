01d71488f8417be6fa253cc91b2211d18c2dd354
youichiro <cinnamon416@gmail.com>
2022-04-17 15:57:49 +0900

    学生情報編集画面

--- app/controllers/students/registrations_controller.rb
+++ app/controllers/students/registrations_controller.rb
@@ -18,9 +18,9 @@ class Students::RegistrationsController < Devise::RegistrationsController
   end
 
   # GET /resource/edit
-  # def edit
-  #   super
-  # end
+  def edit
+    super
+  end
 
   # PUT /resource
   # def update
