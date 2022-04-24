eaedf61c1662264da0d9c18a4235b90e271d7dfa
youichiro <cinnamon416@gmail.com>
2022-04-24 16:50:12 +0900

    seedに追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,41 @@
 BusinessCategory.insert_all([
   { name: 'IT・通信' },
   { name: '建設・建築' },
-  { name: '自動車販売' }
+  { name: '自動車販売' },
 ])
+
+student = Student.create!(
+  email: 'admin1@gmail.com',
+  password: 'password',
+  confirmed_at: Time.zone.now,
+)
+StudentDetail.create!(
+  student: student,
+  name: 'テスト生徒ユーザ',
+  disable_notification: false,
+  birthday: '2030-01-01',
+  school_name: 'ほげ大学',
+  department: 'ほげ学部',
+)
+
+company = Company.create!(
+  name: '株式会社ほげ',
+  company_url: 'https://hoge.com',
+  phone_number: '0000-00-0000',
+  business_category: BusinessCategory.first,
+  address: 'ほげ',
+  business_description: '元気いっぱい',
+  number_of_employee: 1,
+  manager_name: 'youichiro',
+  manager_email: 'admin@gmail.com',
+)
+employee = Employee.create!(
+  email: 'admin2@gmail.com',
+  password: 'password',
+  confirmed_at: Time.zone.now,
+  company: company,
+)
+EmployeeDetail.create!(
+  employee: employee,
+  name: 'テスト企業ユーザ',
+)
