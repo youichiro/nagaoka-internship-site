489f3a131992dc47b867369a640bf24ddeaf2415
youichiro <cinnamon416@gmail.com>
2022-04-24 17:45:28 +0900

    企業ユーザ情報更新

--- app/controllers/employees/registrations_controller.rb
+++ app/controllers/employees/registrations_controller.rb
@@ -2,7 +2,7 @@
 
 class Employees::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   def new
@@ -57,14 +57,14 @@ class Employees::RegistrationsController < Devise::RegistrationsController
   end
 
   # GET /resource/edit
-  # def edit
-  #   super
-  # end
+  def edit
+    super
+  end
 
   # PUT /resource
-  # def update
-  #   super
-  # end
+  def update
+    super
+  end
 
   # DELETE /resource
   # def destroy
@@ -91,9 +91,11 @@ class Employees::RegistrationsController < Devise::RegistrationsController
   end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [
+      employee_detail_attributes: [:id, :name]
+    ])
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
