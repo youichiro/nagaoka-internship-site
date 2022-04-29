d076caaeb842b7d8115e02a5b0e889d37f955c9a
youichiro <cinnamon416@gmail.com>
2022-04-30 02:48:07 +0900

    add accouncement model

---
+++ app/models/announcement.rb
@@ -0,0 +1,6 @@
+class Accouncement < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :contents, presence: true
+
+  belongs_to :announcement_category
+end
--- app/models/announcement_category.rb
+++ app/models/announcement_category.rb
@@ -1,3 +1,5 @@
 class AccouncementCategory < ApplicationRecord
   validates :name, presence: true, uniqueness: true, length: { maximum: 100 }
+
+  has_many :announcements, dependent: :restrict_with_exception
 end
