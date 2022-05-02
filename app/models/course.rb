1137d3aab77c4ffe9a2147c8484e3f033af62f81
youichiro <cinnamon416@gmail.com>
2022-05-02 21:26:30 +0900

    add relation course_category

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,6 +2,7 @@ class Course < ApplicationRecord
   validates :title, presence: true, length: { maximum: 100 }
   validates :event_date, presence: true
 
+  belongs_to :course_category
   has_one_attached :thumbnail
   has_one_attached :video
 end
--- app/models/course_category.rb
+++ app/models/course_category.rb
@@ -1,5 +1,5 @@
 class CourseCategory < ApplicationRecord
   validates :name, presence: true, uniqueness: true, length: { maximum: 100 }
 
-  # has_many :announcements, dependent: :restrict_with_exception
+  has_many :courses, dependent: :restrict_with_exception
 end
