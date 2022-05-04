fad776cebc68d9f46c4bed919b4eb3833e8161cc
youichiro <cinnamon416@gmail.com>
2022-05-04 10:57:01 +0900

    companies.address null: true

---
+++ db/migrate/20220504015354_change_column_null_address_to_companies.rb
@@ -0,0 +1,5 @@
+class ChangeColumnNullAddressToCompanies < ActiveRecord::Migration[7.0]
+  def change
+    change_column_null :companies, :address, true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_04_013653) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_04_015354) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -92,7 +92,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_04_013653) do
     t.string "company_url", default: ""
     t.string "contact_phone_number", null: false
     t.bigint "business_category_id", null: false
-    t.string "address", null: false
+    t.string "address"
     t.text "business_description", default: ""
     t.integer "number_of_employee"
     t.string "contact_name", null: false
