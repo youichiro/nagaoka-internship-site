694c70758820688f63a1a99f6efd4264f19a7772
youichiro <cinnamon416@gmail.com>
2022-05-15 14:35:00 +0900

    カテゴリカラーを表示

--- app/models/announcement_category.rb
+++ app/models/announcement_category.rb
@@ -2,4 +2,8 @@ class AnnouncementCategory < ApplicationRecord
   validates :name, presence: true, uniqueness: true, length: { maximum: 100 }
 
   has_many :announcements, dependent: :restrict_with_exception
+
+  def bg_color
+    color.present? ? color : 'teal'
+  end
 end
--- app/models/course_category.rb
+++ app/models/course_category.rb
@@ -2,4 +2,8 @@ class CourseCategory < ApplicationRecord
   validates :name, presence: true, uniqueness: true, length: { maximum: 100 }
 
   has_many :courses, dependent: :restrict_with_exception
+
+  def bg_color
+    color.present? ? color : 'teal'
+  end
 end
