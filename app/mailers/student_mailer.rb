ef3f50e04281af983153d2aae42fe373b4704513
youichiro <cinnamon416@gmail.com>
2022-05-27 22:16:04 +0900

    インターンシップ申し込みMailer追加

--- app/mailers/student_mailer.rb
+++ app/mailers/student_mailer.rb
@@ -4,4 +4,10 @@ class StudentMailer < ApplicationMailer
     @course = params[:course]
     mail(to: @student.email, subject: '【長岡のしごと場】講座のお申し込み内容のご連絡')
   end
+
+  def internship_order_confirm_mail
+    @student = params[:student]
+    @internship = params[:internship]
+    mail(to: @student.email, subject: '【長岡のしごと場】インターンシップのお申し込み内容のご連絡')
+  end
 end
--- test/mailers/previews/student_mailer_preview.rb
+++ test/mailers/previews/student_mailer_preview.rb
@@ -2,4 +2,8 @@ class StudentMailerPreview < ActionMailer::Preview
   def course_order_confirm_mail
     StudentMailer.with(student: Student.first, course: Course.first).course_order_confirm_mail
   end
+
+  def internship_order_confirm_mail
+    StudentMailer.with(student: Student.first, internship: Internship.first).internship_order_confirm_mail
+  end
 end
