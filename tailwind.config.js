f26c550ef114054e327fc681d076000ded517fcd
youichiro <cinnamon416@gmail.com>
2022-06-04 18:02:22 +0900

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,19 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  theme: {
+    fontFamily: {
+      'section': ['Noto Sans JP', 'sans-serif'],
+    },
+    extends: {
+      colors: {
+        'primary': '#42AAC7',
+        'secondary': '#E6855E',
+        'info': '#CE579B',
+        'warning': '#F9DB57',
+        'danger': '#DA6272',
+      }
+    }
+  }
 }
