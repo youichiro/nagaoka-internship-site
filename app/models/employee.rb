df0a846dc3404ce07f432496e4f78b5d4671a8ff
youichiro <cinnamon416@gmail.com>
2022-06-18 03:28:26 +0900

    validate password

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -6,6 +6,7 @@ class Employee < ApplicationRecord
          :confirmable, :trackable
 
   validates :agreement_terms, allow_nil: false, acceptance: true, on: :create
+  validate :password_complexity
 
   belongs_to :company
   has_one :employee_detail, dependent: :destroy
@@ -13,4 +14,9 @@ class Employee < ApplicationRecord
   has_many :employee_course_orders, dependent: :destroy
 
   accepts_nested_attributes_for :employee_detail
+
+  def password_complexity
+    return if password.blank? || password =~ /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,70}$/
+    errors.add :password, "長さは8〜70文字とし、大文字と小文字と数字と特殊文字をそれぞれ1文字以上含める必要があります。"
+  end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -6,6 +6,7 @@ class Student < ApplicationRecord
          :confirmable, :trackable
 
   validates :agreement_terms, allow_nil: false, acceptance: true, on: :create
+  validate :password_complexity
 
   has_one :student_detail, dependent: :destroy
   has_many :courses, through: :student_course_orders
@@ -16,4 +17,9 @@ class Student < ApplicationRecord
   has_many :internship_orders, dependent: :destroy
 
   accepts_nested_attributes_for :student_detail
+
+  def password_complexity
+    return if password.blank? || password =~ /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,70}$/
+    errors.add :password, "長さは8〜70文字とし、大文字と小文字と数字と特殊文字をそれぞれ1文字以上含める必要があります。"
+  end
 end
